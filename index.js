a7fe9e2f34f0bb2e840ec067fcf371eb2d3191b9
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-24 17:01:04 +0530

    added group chat implementation
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- index.js
+++ index.js
@@ -19,31 +19,52 @@ const io = socketIO(server);
 
 let users = [];
 
-const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) && userId !== null &&
-        users.push({ userId, socketId });
-};
+const addUser = (userId, userName, room, socketId) => {
+
+    const user = { userId, socketId, userName, room }
+    !users.some((user) => user.userId === userId) && userId !== null && users.push(user);
+    return user;
+}
+
+const getUser = (userId) => {
+    let user = users.find((user) => user.userId === userId)
+    return user;
+}
 
 const removeUser = (socketId) => {
     users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-};
+}
 
 io.on("connection", (socket) => {
-    socket.on("addUser", (userId) => {
-        addUser(userId, socket.id);
+
+    socket.on("addUser", (userId, userName, room) => {
+        const user = addUser(userId, userName, room, socket.id);
+        socket.join(user.room);
         io.emit("getUsers", users);
     });
 
-    socket.on("sendMessage", async ({ senderId, receiverId, text }) => {
-        const user = await getUser(receiverId);
-        io.to(user.socketId).emit("getMessage", {
-            senderId,
-            text,
-        });
+    socket.on("sendMessage", async ({ senderId, senderName, receiverId, text, isGroup }) => {
+        if (isGroup) {
+            const user = await getUser(senderId);
+            socket.in(user.room).emit("getMessage", {
+                room: user.room,
+                message: {
+                    senderId,
+                    senderName,
+                    text,
+                }
+            });
+        } else {
+            const user = await getUser(receiverId);
+            io.to(user.socketId).emit("getMessage", {
+                room: "",
+                message: {
+                    senderId,
+                    senderName,
+                    text,
+                }
+            });
+        }
     });
 
     socket.on("disconnect", () => {
